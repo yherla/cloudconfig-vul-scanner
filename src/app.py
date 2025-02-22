fc400884b6b65a4120a686867fca70b381f79384
Jordi Yherla <yhecoll@gmail.com>
2025-02-22 21:38:13 +0100

    enabled CSFR Protect as advised by Sonar

--- src/app.py
+++ src/app.py
@@ -3,13 +3,15 @@ import json
 import csv
 from io import StringIO
 from flask import Flask, render_template, request, redirect, flash, Response, session, url_for
-
+from flask_wtf.csrf import CSRFProtect
 from .scanner import VulnerabilityScanner
 
 template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'templates')
 static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'static')
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+
+csrf = CSRFProtect(app)
 
 app.secret_key = os.environ.get('SECRET_KEY', 'dev-secret-key')
 
@@ -69,7 +71,6 @@ def download_csv():
         ])
     output = si.getvalue()
     si.close()
-    
     return Response(
         output,
         mimetype="text/csv",
